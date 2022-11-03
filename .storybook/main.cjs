1e975316048764b0f0ae47dbf89bd078727bc705
Vanessa <vsmargareth@hotmail.com>
2022-11-03 18:33:16 -0300

    modificação vite

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -15,11 +15,11 @@ module.exports = {
   "features": {
     "storyStoreV7": true
   },
-
   viteFinal: (config, { configType }) => {
     if (configType === 'PRODUCTION') {
-      config.base = '/ignite_lab-DS'
+      config.base = '/ignite_lab-DS/'
     }
+
     return config
   }
 }
